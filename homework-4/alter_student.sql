925eec01bbd229d72f8c5e03463cf5e7578ce640
PolinaWednesday <kekssmail@gmail.com>
2023-11-23 22:47:42 +0300

    База nortwind and student4

--- homework-4/alter_student.sql
+++ homework-4/alter_student.sql
@@ -1,19 +1,37 @@
 -- 1. Создать таблицу student с полями student_id serial, first_name varchar, last_name varchar, birthday date, phone varchar
 
+CREATE TABLE student
+(
+  student_id serial PRIMARY KEY,
+  first_name varchar,
+  last_name varchar,
+  birthday date,
+  phone varchar
+);
 
 -- 2. Добавить в таблицу student колонку middle_name varchar
 
+ALTER TABLE student ADD COLUMN middle_name varchar;
 
 -- 3. Удалить колонку middle_name
 
+ALTER TABLE student DROP COLUMN middle_name;
 
 -- 4. Переименовать колонку birthday в birth_date
 
+ALTER TABLE student RENAME COLUMN birthday TO birth_date;
 
 -- 5. Изменить тип данных колонки phone на varchar(32)
 
+ALTER TABLE student ALTER COLUMN phone TYPE varchar(32);
 
 -- 6. Вставить три любых записи с автогенерацией идентификатора
 
+INSERT INTO student (first_name, last_name, birth_date, phone)
+VALUES ('Paul', 'Wesley', '1982/07/23', '89899898998'),
+('Ian', 'Somerhalder', '1978/12/08', '98988989898'),
+('Nina', 'Dobrev', '1989/01/09', '99998889988');
 
 -- 7. Удалить все данные из таблицы со сбросом идентификатор в исходное состояние
+
+TRUNCATE TABLE student RESTART IDENTITY;
